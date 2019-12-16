a901b081805fe2b1352923d938f659e3ba5148e3
Thornton Rose <thornton.rose@disneystreaming.com>
2019-12-16 11:35:41 -0500

    Fix day 1, part 2.

--- 2019/day01/part1.py
+++ 2019/day01/part1.py
@@ -2,4 +2,4 @@
 from pathlib import Path
 modules = Path("input.txt").read_text().splitlines()
 # print(sum(map(lambda m: (int(m) // 3) - 2, modules)))
-print(sum([ (int(m) // 3) - 2 for m in modules ]))
+print(sum((int(m) // 3) - 2 for m in modules))
--- 2019/day01/part2.py
+++ 2019/day01/part2.py
@@ -7,4 +7,4 @@ def calc_fuel(module):
 
 modules = Path("input.txt").read_text().splitlines()
 # print(sum(map(lambda m: calc_fuel(m), modules)))
-print(sum([ (int(m) // 3) - 2 for m in modules ]))
+print(sum(calc_fuel(m) for m in modules))
